56ed76caf490c7a257b4ba4961a2e923f945c9d9
Alexey19800506 <osiyuk19800506@gmail.com>
2022-06-19 12:46:47 +0300

    task5

--- calculator.js
+++ calculator.js
@@ -23,3 +23,24 @@ export const calc = expression => {
 
     return `${expression} = ${result}`;
 };
+
+export const getSquaredArray = numbers =>
+    numbers.map(num => num * num);
+
+export const getOddNumbers = numbers =>
+    numbers.filter(num => (num % 2 === 1));
+
+export default (a, b) => a + b;
+
+export const getMinSquaredNumber = numbers => {
+    if (!Array.isArray(numbers || numbers.length == 0)) {
+        return null;
+    }
+    let min = Infinity;
+    for (let key in numbers) {
+        if (Math.abs(numbers[key]) < min) {
+            min = Math.abs(numbers[key]);
+        }
+    }
+    return min * min;
+}
--- calculator.test.js
+++ calculator.test.js
@@ -1,4 +1,4 @@
-import { calc } from './calculator.js'
+import getSum, { calc, getSquaredArray, getOddNumbers, getMinSquaredNumber } from './calculator.js'
 
 it('should get subtraction numbers', () => {
     const result = calc('5 - 3');
@@ -18,4 +18,36 @@ it('should get multiplication numbers', () => {
 it('should get division numbers', () => {
     const result = calc('15 / 3');
     expect(result).toEqual('15 / 3 = 5');
+});
+
+
+it('should get squared numbers', () => {
+    const result = getSquaredArray([1, 2, 3]);
+    expect(result).toEqual([1, 4, 9]);
+});
+
+it('should keep odd numbers only', () => {
+    const result = getOddNumbers([1, 2, 3, 4, 5]);
+    expect(result).toEqual([1, 3, 5]);
+});
+
+it('should get sum of numbers', () => {
+    const result = getSum(4, 5);
+    expect(result).toEqual(9);
+});
+
+
+it('should get squared min |number|', () => {
+    const result = getMinSquaredNumber([3, -7, 0.19, 22.9, -33.3]);
+    expect(result).toEqual(0.0361);
+});
+
+it('should get squared min |number|', () => {
+    const result = getMinSquaredNumber([-93, -7, 1, -0.2, -33.3]);
+    expect(result).toEqual(0.04);
+});
+
+it('should get squared min |number|', () => {
+    const result = getMinSquaredNumber([-8, 37, 7, 22.9, -33.3, -1]);
+    expect(result).toEqual(1);
 });
